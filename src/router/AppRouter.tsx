352a430a02e4e9da94eb54b1132a584682a6ac8b
sergio2732 <sergioparra1227@gmail.com>
2024-05-21 12:01:43 -0500

    Router actualization

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -8,9 +8,9 @@ export const AppRouter = () => {
   return (
     <Routes>
       {<Route path="/" element={<Main />} />}
-      <Route path="/example1" element={<Example1 />} />
+      {<Route path="/example1" element={<Example1 />} />}
       {<Route path="/legalDocuments" element={<LegalDocuments />} />}
-      <Route path="*" element={<NoMatch />} />
+      {<Route path="*" element={<NoMatch />} />}
     </Routes>
   );
 };
