01051a058cc2be9d7267a45a89d0e9051ceb8e6a
sergio2732 <sergioparra1227@gmail.com>
2024-05-22 15:07:04 -0500

    Styles of ui react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavBar } from "./components/common";
 import { AppRouter } from "./router/AppRouter";
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
