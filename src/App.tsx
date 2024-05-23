05cf92b1cb5c39f5cd763c3a4fe1369dfbb50c90
sergio2732 <sergioparra1227@gmail.com>
2024-05-23 00:00:14 -0500

    NavBar modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavBar } from "./components/common";
 import { AppRouter } from "./router/AppRouter";
+import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
     <Router>
-      <NavBar/>
-      <AppRouter/>
+      <header id="Nav">
+        <NavBar />
+      </header>
+      <main>
+        <AppRouter />
+      </main>
     </Router>
   );
 }
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,41 +1,50 @@
-import { Link, NavLink } from "react-router-dom";
+import '@aws-amplify/ui-react/styles.css';
+import { Breadcrumbs } from '@aws-amplify/ui-react';
+
+const breadcrumbs = [
+  {
+    href: '/',
+    text: 'Home',
+  },
+  {
+    href: '/example1',
+    text: 'Example1',
+  },
+  {
+    href: '/legalDocuments',
+    text: 'Legal Documents',
+  },
+  {
+    text: '',
+    isCurrent: true,      
+  },
+];
 
 export const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Legal Management App - Amplify 
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink className="nav-link " to="/Example1">
-                Example 1
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link " to="/legalDocuments">
-                Legal Documents
-              </NavLink>
-            </li>
-          </ul>
-          <Link to="/" className="btn btn-outline-primary">
-            Main page
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <Breadcrumbs.Container
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        padding: '10px 0',
+      }}
+    >
+      {breadcrumbs.map(({ href, text, isCurrent }, idx) => (
+        <Breadcrumbs.Item key={`${href}${idx}`}>
+          <Breadcrumbs.Link
+            fontWeight="bold"
+            textDecoration="underline"
+            fontSize={'1.5rem'}
+            color={'black'}
+            href={href}
+            isCurrent={isCurrent}
+          >
+            {text}
+          </Breadcrumbs.Link>
+        </Breadcrumbs.Item>
+      ))}
+    </Breadcrumbs.Container>
   );
 };
